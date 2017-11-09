57e21781eee497572b73ca70d0613fa671ed143c
Peter Alexay <peter.alexay@rohde-schwarz.com>
2017-11-09 12:46:26 +0100

    Changed: modified pattern drawing for dot-small pattern

--- src/shapes/dot-small.js
+++ src/shapes/dot-small.js
@@ -2,28 +2,28 @@ import Shape from './shape';
 
 export default class Dot extends Shape {
   drawTile() {
-    const halfSize = this._size / 2;
-
+    const doubleSize = this._size * 2;
     this._context.beginPath();
 
     this.setFillProps();
 
-    this.drawDot();
-    this.drawDot(halfSize, halfSize);
+    //this.drawDot();
+    this.drawDot(doubleSize, doubleSize);
 
     this._context.fill();
 
     return this._canvas;
   }
 
-  drawDot(offsetX = 0, offsetY = 0, diameter = this._size / 10) {
+  drawDot(offsetX = 0, offsetY = 0) {
     const size = this._size;
-    const quarterSize = size / 8;
-    const x = quarterSize + offsetX;
-    const y = quarterSize + offsetY;
+    //const quarterSize = size / 8;
+    const x = size + offsetX;
+    const y = size + offsetY;
 
-    this._context.moveTo(x + quarterSize , y);
-    this._context.arc(x, y, diameter, 0, 2 * Math.PI);
+    this._context.moveTo(x , y);
+    //this._context.arc(x, y, diameter, 0, 2 * Math.PI);
+    this._context.fillRect(x, y, size, size)
 
     this._context.closePath();
   }
